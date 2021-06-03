6926676fc1232a81ef817c8d09303258c9d72de0
Brnjak6 <68736438+Brnjak6@users.noreply.github.com>
2021-06-03 17:45:25 +0200

    Tabbed components update

--- script.js
+++ script.js
@@ -1,110 +1,124 @@
-'use strict';
+"use strict";
 
-const burgerIcon = document.querySelector('.sidebar__burger');
-const burgerIconArrs = document.querySelectorAll('.sidebar__burger-svg');
-const burgerMenu = document.querySelector('.sidebar__open');
-const burgerClose = document.querySelector('.sidebar__open-close-btn');
-const burgerItems = document.querySelector('.sidebar__items');
+const burgerIcon = document.querySelector(".sidebar__burger");
+const burgerIconArrs = document.querySelectorAll(".sidebar__burger-svg");
+const burgerMenu = document.querySelector(".sidebar__open");
+const burgerClose = document.querySelector(".sidebar__open-close-btn");
+const burgerItems = document.querySelector(".sidebar__items");
 
 // Events
 
-burgerIcon.addEventListener('mouseover', function () {
-    burgerIconArrs.forEach(arr => arr.style.transform = 'scale(1.1)')
-})
-burgerIcon.addEventListener('mouseout', function () {
-    burgerIconArrs.forEach(arr => arr.style.transform = 'scale(1)')
-})
+const noScroll = () => {
+  window.scrollTo(0, 0);
+};
 
-burgerIcon.addEventListener('click', function () {
-    burgerMenu.classList.add('visible');
-})
+burgerIcon.addEventListener("mouseover", function () {
+  burgerIconArrs.forEach((arr) => (arr.style.transform = "scale(1.1)"));
+});
+burgerIcon.addEventListener("mouseout", function () {
+  burgerIconArrs.forEach((arr) => (arr.style.transform = "scale(1)"));
+});
 
-burgerClose.addEventListener('click', function () {
-    burgerMenu.classList.remove('visible')
-})
+burgerIcon.addEventListener("click", function () {
+  burgerMenu.classList.add("visible");
+  window.addEventListener("scroll", noScroll);
+});
 
+burgerClose.addEventListener("click", function () {
+  burgerMenu.classList.remove("visible");
+  window.removeEventListener("scroll", noScroll);
+});
 
-burgerItems.addEventListener('click', function (e) {
-    e.preventDefault();
+burgerItems.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    if (e.target.classList.contains('sidebar__item-a')) {
-        const id = e.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
-    }
-})
+  if (e.target.classList.contains("sidebar__item-a")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
 //Tabbed component
-const tabs = document.querySelectorAll('.adv-buttons--btn');
-const tabsContainer = document.querySelector('.adv-buttons');
-const tabsContent = document.querySelectorAll('.advantages--description');
-const tabsLines = document.querySelectorAll('.advantages--description-line');
+const tabs = document.querySelectorAll(".adv-buttons--btn");
+const tabsContainer = document.querySelector(".adv-buttons");
+const tabsContent = document.querySelectorAll(".advantages--description");
+const tabsLines = document.querySelectorAll(".advantages--description-line");
 
-tabsContainer.addEventListener('click', function (e) {
-    const clicked = e.target.closest('.adv-buttons--btn');
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".adv-buttons--btn");
 
-    if (clicked) {
-        tabs.forEach(t => t.classList.remove('adv-buttons--active'));
-        tabsContent.forEach(description => description.classList.remove('advantages--description-active'));
-        tabsLines.forEach(line => line.classList.remove('advantages--description-line--active'))
+  if (clicked) {
+    tabs.forEach((t) => t.classList.remove("adv-buttons--active"));
+    tabsContent.forEach((description) =>
+      description.classList.remove("advantages--description-active")
+    );
+    tabsLines.forEach((line) =>
+      line.classList.remove("advantages--description-line--active")
+    );
 
-        clicked.classList.add('adv-buttons--active');
+    clicked.classList.add("adv-buttons--active");
 
-        document.querySelector(`.advantages--description-${clicked.dataset.tab}`).classList.add('advantages--description-active');
+    document
+      .querySelector(`.advantages--description-${clicked.dataset.tab}`)
+      .classList.add("advantages--description-active");
 
-        document.querySelector(`.advantages--description-line-${clicked.dataset.tab}`).classList.add('advantages--description-line--active')
+    document
+      .querySelector(`.advantages--description-line-${clicked.dataset.tab}`)
+      .classList.add("advantages--description-line--active");
+  }
+});
 
-    }
+burgerItems.addEventListener("click", function (e) {
+  e.preventDefault();
 
-})
-
-burgerItems.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    if (e.target.classList.contains('sidebar__item')) {
-        const id = e.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-        burgerMenu.classList.remove('visible')
-
-    }
-})
+  if (e.target.classList.contains("sidebar__item")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    burgerMenu.classList.remove("visible");
+  }
+});
 
 //Mouseover and mouseout
-const footer = document.querySelector('.footer')
+const footer = document.querySelector(".footer");
 
 const footerFocus = function (e, opacity) {
-    if (e.target.classList.contains('options__link')) {
-        const link = e.target;
-        const siblings = link.closest('.options').querySelectorAll('.options__link');
+  if (e.target.classList.contains("options__link")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".options")
+      .querySelectorAll(".options__link");
 
-        siblings.forEach(s => {
-            if (s !== link) s.style.opacity = opacity;
-        })
-    }
-}
+    siblings.forEach((s) => {
+      if (s !== link) s.style.opacity = opacity;
+    });
+  }
+};
 
-footer.addEventListener('mouseover', function (e) {
-    footerFocus(e, 0.5)
-})
-footer.addEventListener('mouseout', function (e) {
-    footerFocus(e, 1)
-})
+footer.addEventListener("mouseover", function (e) {
+  footerFocus(e, 0.5);
+});
+footer.addEventListener("mouseout", function (e) {
+  footerFocus(e, 1);
+});
 // Mouseover burgerMenu
-const sidebarItems = document.querySelector('.sidebar__open');
+const sidebarItems = document.querySelector(".sidebar__open");
 
 const burgerItemFocus = function (e, opacity) {
-    if (e.target.classList.contains('sidebar__link')) {
-        const link = e.target;
-        const siblings = link.closest('.sidebar__open').querySelectorAll('.sidebar__link');
+  if (e.target.classList.contains("sidebar__link")) {
+    const link = e.target;
+    const siblings = link
+      .closest(".sidebar__open")
+      .querySelectorAll(".sidebar__link");
 
-        siblings.forEach(s => {
-            if (s !== link) s.style.opacity = opacity;
-        })
-    }
-}
+    siblings.forEach((s) => {
+      if (s !== link) s.style.opacity = opacity;
+    });
+  }
+};
 
-sidebarItems.addEventListener('mouseover', function (e) {
-    burgerItemFocus(e, 0.5)
-})
-sidebarItems.addEventListener('mouseout', function (e) {
-    burgerItemFocus(e, 1)
-})
+sidebarItems.addEventListener("mouseover", function (e) {
+  burgerItemFocus(e, 0.5);
+});
+sidebarItems.addEventListener("mouseout", function (e) {
+  burgerItemFocus(e, 1);
+});
